c9a78aaadbdb96427e999b7e73bcde496297fe9d
Your Name <you@example.com>
2021-05-22 19:14:41 +0530

    'day3'

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCuzwJlqr2-3SV4ovsiZYgv4-6ETQ0gnNw",
+    authDomain: "yt-11416.firebaseapp.com",
+    projectId: "yt-11416",
+    storageBucket: "yt-11416.appspot.com",
+    messagingSenderId: "921115308249",
+    appId: "1:921115308249:web:fc6f45f3e8f86eba420ec8",
+    measurementId: "G-PGDGL9Y63K"
+  };
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,17 +2,36 @@ import Image from "next/image"
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid"
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 
 function Products({ id, title, price, description, category, image }) {
+    const dispatch = useDispatch();
+
     const [rating] = useState(
-        Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1 )) + MIN_RATING
+        Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
     );
 
     const [hasPrime] = useState(Math.random() < 0.5);
+
+    const addItemToBasket = () => {
+        const product = {
+            id,
+            title,
+            price,
+            rating,
+            description,
+            category,
+            image,
+            hasPrime
+        };
+        // sending product
+        dispatch(addToBasket(product))
+    }
 
     return (
         <div className="relative flex flex-col m-5 bg-white z-30 p-10">
@@ -22,9 +41,9 @@ function Products({ id, title, price, description, category, image }) {
             <div className="flex">
                 {Array(rating).fill().map((_, i) => (
                     <StarIcon className="h-5 text-yellow-500" />
-                )) }
+                ))}
             </div>
-            <p className="text-x my-2 line-clamp-2" >{description}</p>
+            <p className="text-xs my-2 line-clamp-2" >{description}</p>
             <div className="mb-5">
                 <Currency quantity={price} currency="INR" />
             </div>
@@ -35,7 +54,7 @@ function Products({ id, title, price, description, category, image }) {
                 </div>
             )}
 
-            <button className="mt-auto button">Add to Basket</button>
+            <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button>
         </div>
     )
 }
